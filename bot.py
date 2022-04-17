e33e68cc32e1d7ec3664b16504a3a02f20b65b30
xyquadrat <mail@xyquadrat.ch>
2022-04-17 17:59:02 +0200

    Implement very basic scraper

--- bot.py
+++ bot.py
@@ -1,15 +1,68 @@
 import discord
+import os
 from discord.ext import commands
-from config import token
+import config
+import sql
+import logging
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix="|")
+sql_con = sql.connect()
+logging.basicConfig(level=logging.INFO)
+
+UPVOTE_ID = 747783377662378004
+DOWNVOTE_ID = 758262252699779073
+
 
 @bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(bot))
+    print("Logged in as {0.user}".format(bot))
+
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("pong")
 
-bot.run(token)
+
+@bot.command()
+async def scrape(ctx):
+    async for message in ctx.channel.history(limit=None):
+        for attachment in message.attachments:
+            orig_name, extension = os.path.splitext(attachment.filename)
+            filename = str(attachment.id) + extension
+            logging.info("Processing attachment %s...", filename)
+
+            if sql.exists_record(sql_con, filename):
+                logging.info("Attachment %s already exists in DB, skipping", filename)
+                continue
+
+            save_path = os.path.join(config.download_path, filename)
+
+            await attachment.save(save_path)
+
+            upvoteReaction = next(
+                (r for r in message.reactions if hash(r) == UPVOTE_ID), None
+            )
+            downvoteReaction = next(
+                (r for r in message.reactions if hash(r) == DOWNVOTE_ID), None
+            )
+
+            upvotes = None
+            downvotes = None
+            score = None
+
+            if upvoteReaction is not None and downvoteReaction is not None:
+                upvotes = upvoteReaction.count
+                downvote = downvoteReaction.count
+                score = upvotes - downvote
+
+            sql.insert_meme(
+                sql_con,
+                filename,
+                score,
+                message.author.name + "#" + message.author.discriminator,
+            )
+
+            logging.info("Inserted attachment %s into DB", filename)
+
+
+bot.run(config.token)
---
+++ sql.py
@@ -0,0 +1,25 @@
+import sqlite3
+import typing
+
+
+def connect() -> sqlite3.Connection:
+    con = sqlite3.connect("memes.db")
+    return con
+
+
+def insert_meme(con: sqlite3.Connection, name: str, score, author: str):
+    con.execute(
+        """INSERT INTO MediaItem(Filename, Score, Author)
+           VALUES(?,?,?)""",
+        (name, score, author),
+    )
+    con.commit()
+
+
+def exists_record(con: sqlite3.Connection, filename: str) -> bool:
+    row = con.execute(
+        """SELECT EXISTS(SELECT 1 FROM MediaItem WHERE Filename = :name)""",
+        {"name": filename},
+    ).fetchone()[0]
+
+    return row
